7f54aaca8b6e30d3d9bf4d2315b2fb13d7919e5f
Ty Peterson <tywpeterson@gmail.com>
2024-07-29 12:42:03 -0600

    .

--- cmd/init.go
+++ cmd/init.go
@@ -19,59 +19,80 @@ func Init() error {
 		return errors.New("Project already initialized, run 'gittier update' instead")
 	}
 
-	// create and switch to FileTreeBranch and defer switching back to original branch
-	originalBranch, err := core.SwitchToFileTreeBranch()
+	// create FileTreeBranch
+	currentBranch, err := core.GetCurrentBranch()
 	if err != nil {
+		fmt.Println("failed to get current branch")
+		return err
+	}
+
+	// stash any changes if needed
+	needToStash, err := core.NeedToStash(currentBranch)
+	if err != nil {
+		fmt.Println("failed to check if need to stash")
+		return err
+	}
+
+	if needToStash {
+		if err := core.Stash(); err != nil {
+			fmt.Println("failed to stash")
+			return err
+		}
+
+		defer func() {
+			if err := core.StashPop(); err != nil {
+				fmt.Println("failed to pop stash")
+			}
+
+		}()
+	}
+
+	// switch to FileTreeBranch
+	if err := core.SwitchToBranch(core.FileTreeBranch); err != nil {
 		fmt.Println("failed to switch to filetree branch")
 		return err
 	}
-	// defer core.SwitchToBranch(originalBranch)
-	// defer core.StashPop()
 
-	// switch back to original branch
-	if err := core.SwitchToBranch(originalBranch); err != nil {
-		fmt.Println("failed to switch back to original branch")
-		return err
-	}
+	// defer switching back to current branch
+	defer func() {
+		if err := core.SwitchToBranch(currentBranch); err != nil {
+			fmt.Println("failed to switch back to current branch")
+		}
+	}()
 
-	// stash pop
-	if err := core.StashPop(); err != nil {
-		fmt.Println("failed to stash pop")
-		return err
-	}
 	// get current commit hash
-	// commitHash, err := core.GetCurrentCommitHash()
-	// if err != nil {
-	// 	fmt.Println("failed to get current commit hash")
-	// 	return err
-	// }
+	commitHash, err := core.GetCurrentCommitHash()
+	if err != nil {
+		fmt.Println("failed to get current commit hash")
+		return err
+	}
 
 	// get FileTree from ls-tree
-	// fileTree, err := core.GetFileTreeFromLsTree()
-	// if err != nil {
-	// 	fmt.Println("failed to get file tree from ls-tree")
-	// 	return err
-	// }
+	fileTree, err := core.GetFileTreeFromLsTree()
+	if err != nil {
+		fmt.Println("failed to get file tree from ls-tree")
+		return err
+	}
 
-	// fileTree.CommitHash = commitHash
+	fileTree.CommitHash = commitHash
 
 	// write FileTree to filetree.yaml
-	// if err := core.WriteFileTreeToYaml(fileTree, "filetree.yaml"); err != nil {
-	// 	fmt.Println("failed to write filetree.yaml")
-	// 	return err
-	// }
+	if err := core.WriteFileTreeToYaml(fileTree, "filetree.yaml"); err != nil {
+		fmt.Println("failed to write filetree.yaml")
+		return err
+	}
 
 	// create .gitattributes file
-	// if err := core.CreateGitAttributes(); err != nil {
-	// 	fmt.Println("failed to create .gitattributes")
-	// 	return err
-	// }
+	if err := core.CreateGitAttributes(); err != nil {
+		fmt.Println("failed to create .gitattributes")
+		return err
+	}
 
-	// stage and commit filetree.yaml to FileTreeBranch
-	// if err := core.StageAndCommit(".", "Initialize filetree.yaml"); err != nil {
-	// 	fmt.Println("failed to stage and commit filetree.yaml")
-	// 	return err
-	// }
+	// stage and commit filetree.yaml and .gitattributes to FileTreeBranch
+	if err := core.StageAndCommit(".", "Initialize filetree.yaml and .gitattributes"); err != nil {
+		fmt.Println("failed to stage and commit filetree.yaml and .gitattributes")
+		return err
+	}
 
 	return nil
 }
