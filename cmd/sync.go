3a4cd8010923c17b94d2f837a5f06eeea91869e0
Ty Peterson <tywpeterson@gmail.com>
2024-07-29 13:11:29 -0600

    .

--- cmd/sync.go
+++ cmd/sync.go
@@ -48,6 +48,8 @@ func Sync() error {
 
 	// get diff between commit hash of filetree.yaml and main
 	diffOutput, err := core.GetDiffOutput(oldFileTree.CommitHash)
+	// output diffOutput to console for debugging reasons
+	fmt.Println(diffOutput)
 	if err != nil {
 		return fmt.Errorf("failed to get diff output: %w", err)
 	}
