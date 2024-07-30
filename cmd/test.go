186fc4ae67e22b31d8d1fa28fb33de78147ecbb1
Ty Peterson <tywpeterson@gmail.com>
2024-07-30 13:49:56 -0600

    changing test output

--- cmd/test.go
+++ cmd/test.go
@@ -2,23 +2,11 @@ package cmd
 
 import (
 	"fmt"
-
-	"github.com/TyPeterson/Gittier/core"
+	// "github.com/TyPeterson/Gittier/core"
 )
 
 func Test() error {
-	fmt.Println("Testing: NeedToStash")
-	currentBranch, err := core.GetCurrentBranch()
-	if err != nil {
-		return fmt.Errorf("failed to get current branch: %w", err)
-	}
-
-	needToStash, err := core.NeedToStash(currentBranch)
-	if err != nil {
-		return fmt.Errorf("failed to check if need to stash: %w", err)
-	}
-
-	fmt.Println("Need to stash:", needToStash)
+	fmt.Println("testing output")
 
 	return nil
 }
